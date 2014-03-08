3f2b50d48e920fb9454d14666087a419de55f964
albertonavarro <alberto.cartagena@gmail.com>
2014-03-08 00:46:17 +0000

    Getting there with wadl

---
+++ warEndpoint/src/main/java/com/navid/trafalgar/recordserverendpoints/RankingImpl.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.navid.trafalgar.recordserverendpoints;
+
+import com.navid.recordserver.MillestoneRecord;
+import com.navid.recordserver.MillestoneRecordAddRequest;
+import com.navid.recordserver.MillestoneRecordAddResponse;
+import com.navid.recordserver.v1.RankingResource;
+
+/**
+ *
+ * @author anf
+ */
+public class RankingImpl implements RankingResource {
+
+    @Override
+    public MillestoneRecordAddResponse post(MillestoneRecordAddRequest millestonerecordaddrequest) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public MillestoneRecord getId(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
