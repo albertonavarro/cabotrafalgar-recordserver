a2e3b1a2842903fc350c7397e07799fc04115551
vero <vero@artifactory02.navid>
2014-05-24 23:59:02 +0100

    simplifying

--- component-test/src/test/java/com/navid/recordserver/BaseIT.java
+++ component-test/src/test/java/com/navid/recordserver/BaseIT.java
@@ -1,7 +1,5 @@
 package com.navid.recordserver;
 
-import com.navid.login.CreateTokenRequest;
-import com.navid.login.UserCommands;
 import com.navid.recordserver.v1.RankingResource;
 import javax.annotation.Resource;
 import org.slf4j.Logger;
@@ -16,27 +14,16 @@ import org.testng.annotations.Test;
  * @author vero
  */
 @Test
-@ContextConfiguration(locations = {"classpath:conf/config-main.xml", "classpath:test-lazylogin-client.xml"})
+@ContextConfiguration(locations = {"classpath:conf/config-main.xml"})
 public class BaseIT extends AbstractTestNGSpringContextTests {
     private static final Logger LOGGER = LoggerFactory.getLogger(BaseIT.class);
-    
-    @Resource(name = "test.clientUserLazyLogin")
-    protected UserCommands userCommandsClient;
     
     @Resource(name = "recordsure.rankingendpoint")
     protected RankingResource rankingService;
     
-    protected String token;
-    
     @BeforeClass
-    public void initToken() {
-        CreateTokenRequest request = new CreateTokenRequest() {{
-            setEmail("somemail@somedomain.com");
-        }};
+    public void init() {
         
-        token = userCommandsClient.createToken(request).getSessionid().getSessionid();
-        
-        LOGGER.info("Got user token: " + token);
     }
     
     
