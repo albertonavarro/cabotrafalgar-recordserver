897d78d9b7214b064e6c3cc9b0e660eca6abcbb1
casa <casa@localhost>
2014-06-14 01:00:36 +0100

    improving recordserver functionality

--- integration-test/src/test/java/com/navid/recordserver/AddRecordIT.java
+++ integration-test/src/test/java/com/navid/recordserver/AddRecordIT.java
@@ -2,6 +2,8 @@ package com.navid.recordserver;
 
 import com.navid.recordserver.v1.AddRecordRequest;
 import com.navid.recordserver.v1.AddRecordResponse;
+import static org.hamcrest.CoreMatchers.equalTo;
+import org.hamcrest.MatcherAssert;
 import org.testng.annotations.Test;
 
 /**
@@ -13,11 +15,11 @@ public class AddRecordIT extends BaseIT {
     @Test
     public void smokeIT() {
         AddRecordRequest addRecordRequest = new AddRecordRequest();
-        addRecordRequest.setId(1);
-        addRecordRequest.setPayload("{\"id\":12, \"payload\": \"{\\\"version\\\":1,\\\"header\\\":{\\\"map\\\":\\\"Games/Millestone2/Millestone2-1_1.json2\\\",\\\"shipModel\\\":\\\"ShipModelOneX\\\"},\\\"stepRecordList\\\":[{\\\"position\\\":{\\\"x\\\":0.0,\\\"y\\\":0.0,\\\"z\\\":0.0},\\\"rotation\\\":{\\\"x\\\":0.0,\\\"y\\\":0.0,\\\"z\\\":0.0,\\\"w\\\":1.0},\\\"timestamp\\\":0.13044842,\\\"eventList\\\":[]},{\\\"position\\\":{\\\"x\\\":-267.15237,\\\"y\\\":0.0,\\\"z\\\":-784.9582},\\\"rotation\\\":{\\\"x\\\":-0.08990571,\\\"y\\\":-0.89595354,\\\"z\\\":0.21214685,\\\"w\\\":-0.3796915},\\\"timestamp\\\":61.56301,\\\"eventList\\\":[\\\"MILLESTONE_REACHED\\\"]}]}\"}");        
+        addRecordRequest.setPayload("{\"version\":1,\"header\":{\"map\":\"" + "one" + "\",\"shipModel\":\"ShipModelOneX\"},\"stepRecordList\":[{\"position\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"rotation\":{\"x\":0.0,\"y\":0.0,\"z\":0.0,\"w\":1.0},\"timestamp\":0.13044842,\"eventList\":[]},{\"position\":{\"x\":-267.15237,\"y\":0.0,\"z\":-784.9582},\"rotation\":{\"x\":-0.08990571,\"y\":-0.89595354,\"z\":0.21214685,\"w\":-0.3796915},\"timestamp\":61.56301,\"eventList\":[\"MILLESTONE_REACHED\"]}]}");
         
         AddRecordResponse response = recordServerClient.post(addRecordRequest);
   
+        MatcherAssert.assertThat(response.getPosition(), equalTo(1));
     }
     
 }
