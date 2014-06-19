8dd5035e3e52705f636e7cbf77a456d5cdcdb42f
casa <casa@localhost>
2014-06-19 22:30:38 +0100

    added position to map searchs

--- component-test/src/test/java/com/navid/recordserver/AddRecordIT.java
+++ component-test/src/test/java/com/navid/recordserver/AddRecordIT.java
@@ -25,7 +25,7 @@ public class AddRecordIT extends BaseIT {
     }
 
     @Test
-    public void addMoreRecords() {
+    public void getByMap() {
         AddRecordRequest addRecordRequest = new AddRecordRequest();
         addRecordRequest.setPayload("{\"version\":1,\"header\":{\"map\":\"" + "addMoreRecords" + "\",\"shipModel\":\"ShipModelOneX\"},\"stepRecordList\":[{\"position\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"rotation\":{\"x\":0.0,\"y\":0.0,\"z\":0.0,\"w\":1.0},\"timestamp\":0.13044842,\"eventList\":[]},{\"position\":{\"x\":-267.15237,\"y\":0.0,\"z\":-784.9582},\"rotation\":{\"x\":-0.08990571,\"y\":-0.89595354,\"z\":0.21214685,\"w\":-0.3796915},\"timestamp\":61.56301,\"eventList\":[\"MILLESTONE_REACHED\"]}]}");
 
@@ -34,13 +34,38 @@ public class AddRecordIT extends BaseIT {
         AddRecordRequest addRecordRequest2 = new AddRecordRequest();
         addRecordRequest.setPayload("{\"version\":1,\"header\":{\"map\":\"" + "addMoreRecords" + "\",\"shipModel\":\"ShipModelOneX\"},\"stepRecordList\":[{\"position\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"rotation\":{\"x\":0.0,\"y\":0.0,\"z\":0.0,\"w\":1.0},\"timestamp\":0.13044842,\"eventList\":[]},{\"position\":{\"x\":-267.15237,\"y\":0.0,\"z\":-784.9582},\"rotation\":{\"x\":-0.08990571,\"y\":-0.89595354,\"z\":0.21214685,\"w\":-0.3796915},\"timestamp\":62.0,\"eventList\":[\"MILLESTONE_REACHED\"]}]}");
 
-        rankingService.post(addRecordRequest);
+        rankingService.post(addRecordRequest2);
 
         GetMapRecordsResponse searchResult = rankingService.getMapsmap("addMoreRecords");
         
         MatcherAssert.assertThat(searchResult.getRankingEntry().size(), equalTo(2));
         MatcherAssert.assertThat(searchResult.getRankingEntry().get(0).getPosition(), equalTo(1));
         MatcherAssert.assertThat(searchResult.getRankingEntry().get(1).getPosition(), equalTo(2));
+    }
+    
+    @Test
+    public void getByUser() {
+        AddRecordRequest addRecordRequest = new AddRecordRequest();
+        addRecordRequest.setPayload("{\"version\":1,\"header\":{\"map\":\"" + "getByUser1" + "\",\"shipModel\":\"ShipModelOneX\"},\"stepRecordList\":[{\"position\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"rotation\":{\"x\":0.0,\"y\":0.0,\"z\":0.0,\"w\":1.0},\"timestamp\":0.13044842,\"eventList\":[]},{\"position\":{\"x\":-267.15237,\"y\":0.0,\"z\":-784.9582},\"rotation\":{\"x\":-0.08990571,\"y\":-0.89595354,\"z\":0.21214685,\"w\":-0.3796915},\"timestamp\":61.56301,\"eventList\":[\"MILLESTONE_REACHED\"]}]}");
+        rankingService.post(addRecordRequest);
+
+        AddRecordRequest addRecordRequest2 = new AddRecordRequest();
+        addRecordRequest.setPayload("{\"version\":1,\"header\":{\"map\":\"" + "getByUser2" + "\",\"shipModel\":\"ShipModelOneX\"},\"stepRecordList\":[{\"position\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"rotation\":{\"x\":0.0,\"y\":0.0,\"z\":0.0,\"w\":1.0},\"timestamp\":0.13044842,\"eventList\":[]},{\"position\":{\"x\":-267.15237,\"y\":0.0,\"z\":-784.9582},\"rotation\":{\"x\":-0.08990571,\"y\":-0.89595354,\"z\":0.21214685,\"w\":-0.3796915},\"timestamp\":62.0,\"eventList\":[\"MILLESTONE_REACHED\"]}]}");
+        rankingService.post(addRecordRequest2);
+        
+        AddRecordRequest addRecordRequest3 = new AddRecordRequest();
+        addRecordRequest3.setPayload("{\"version\":1,\"header\":{\"map\":\"" + "getByUser1" + "\",\"shipModel\":\"ShipModelOneX\"},\"stepRecordList\":[{\"position\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"rotation\":{\"x\":0.0,\"y\":0.0,\"z\":0.0,\"w\":1.0},\"timestamp\":0.13044842,\"eventList\":[]},{\"position\":{\"x\":-267.15237,\"y\":0.0,\"z\":-784.9582},\"rotation\":{\"x\":-0.08990571,\"y\":-0.89595354,\"z\":0.21214685,\"w\":-0.3796915},\"timestamp\":61.56301,\"eventList\":[\"MILLESTONE_REACHED\"]}]}");
+        rankingService.post(addRecordRequest3);
+
+        AddRecordRequest addRecordRequest4 = new AddRecordRequest();
+        addRecordRequest4.setPayload("{\"version\":1,\"header\":{\"map\":\"" + "getByUser2" + "\",\"shipModel\":\"ShipModelOneX\"},\"stepRecordList\":[{\"position\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"rotation\":{\"x\":0.0,\"y\":0.0,\"z\":0.0,\"w\":1.0},\"timestamp\":0.13044842,\"eventList\":[]},{\"position\":{\"x\":-267.15237,\"y\":0.0,\"z\":-784.9582},\"rotation\":{\"x\":-0.08990571,\"y\":-0.89595354,\"z\":0.21214685,\"w\":-0.3796915},\"timestamp\":62.0,\"eventList\":[\"MILLESTONE_REACHED\"]}]}");
+        rankingService.post(addRecordRequest4);
+    
+    }
+    
+    @Test
+    public void getById() {
+    
     }
 
 }
