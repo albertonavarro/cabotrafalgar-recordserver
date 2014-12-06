4d653e17f9b3f18b7722da8849f6bf7d970a7bff
Alberto Navarro <alberto.cartagena@gmail.com>
2014-12-06 23:44:36 +0000

    fixing integration test

--- integration-test/src/test/java/com/navid/recordserver/AddRecordIT.java
+++ integration-test/src/test/java/com/navid/recordserver/AddRecordIT.java
@@ -20,7 +20,7 @@ public class AddRecordIT extends BaseIT {
     @Test
     public void smokeIT() {
         AddRecordRequest addRecordRequest = new AddRecordRequest();
-        addRecordRequest.setPayload("{\"version\":1,\"header\":{\"map\":\"" + "one" + "\",\"shipModel\":\"ShipModelOneX\"},\"stepRecordList\":[{\"position\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"rotation\":{\"x\":0.0,\"y\":0.0,\"z\":0.0,\"w\":1.0},\"timestamp\":0.13044842,\"eventList\":[]},{\"position\":{\"x\":-267.15237,\"y\":0.0,\"z\":-784.9582},\"rotation\":{\"x\":-0.08990571,\"y\":-0.89595354,\"z\":0.21214685,\"w\":-0.3796915},\"timestamp\":61.56301,\"eventList\":[\"MILLESTONE_REACHED\"]}]}");
+        addRecordRequest.setPayload("{\"version\":1,\"header\":{\"map\":\"" + "one" + "\",\"shipModel\":\"ShipModelOneY\"},\"stepRecordList\":[{\"position\":{\"x\":0.0,\"y\":0.0,\"z\":0.0},\"rotation\":{\"x\":0.0,\"y\":0.0,\"z\":0.0,\"w\":1.0},\"timestamp\":0.13044842,\"eventList\":[]},{\"position\":{\"x\":-267.15237,\"y\":0.0,\"z\":-784.9582},\"rotation\":{\"x\":-0.08990571,\"y\":-0.89595354,\"z\":0.21214685,\"w\":-0.3796915},\"timestamp\":61.56301,\"eventList\":[\"MILLESTONE_REACHED\"]}]}");
         
         AddRecordResponse response = recordServerClient.post(addRecordRequest);
   
