b703c548683d0ce3f704ca5dc883b5738255e427
albertonavarro <alberto.cartagena@gmail.com>
2016-11-17 21:37:00 +0000

    Fixing travis build

--- springboot/src/main/java/com/navid/recordserver/springboot/controllers/RankingController.java
+++ springboot/src/main/java/com/navid/recordserver/springboot/controllers/RankingController.java
@@ -1,10 +1,10 @@
 package com.navid.recordserver.springboot.controllers;
 
 import com.google.common.base.Function;
-import com.navid.recordserverapi.server.api.IdApi;
-import com.navid.recordserverapi.server.api.RankingApi;
-import com.navid.recordserverapi.server.api.ShipApi;
-import com.navid.recordserverapi.server.model.*;
+import com.navid.codegen.recordserver.api.IdApi;
+import com.navid.codegen.recordserver.api.RankingApi;
+import com.navid.codegen.recordserver.api.ShipApi;
+import com.navid.codegen.recordserver.model.*;
 import com.navid.trafalgar.recordserver.persistence.CandidateInfo;
 import com.navid.trafalgar.recordserver.persistence.CandidateRecord;
 import com.navid.trafalgar.recordserver.services.RecordServerService;
@@ -38,47 +38,42 @@ public class RankingController implements IdApi, ShipApi, RankingApi {
 
         final CandidateRecord result = recordServerServices.getEntryById(id);
 
-        return new ResponseEntity<MapEntry>(new MapEntry().id(id).payload(result.getPayload()), HttpStatus.OK);
+        return new ResponseEntity<>(new MapEntry().id(id).payload(result.getPayload()), HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<NewMapEntryResponse> postRanking(
+            @ApiParam(value = "New ranking request." ,required=true ) @RequestBody NewMapEntryRequest body) {
+
+        logger.debug("postRanking invoked");
+
+        final CandidateInfo uploadedCandidate = recordServerServices.addEntry(body.getPayload());
+
+        return new ResponseEntity<>( new NewMapEntryResponse().id(uploadedCandidate.getId()).position(1).status("OK").verified(true), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<RankingEntry> getByShipAndMap(
             @ApiParam(value = "TODO",required=true ) @PathVariable("map") String map,
             @ApiParam(value = "TODO",required=true ) @PathVariable("ship") String ship) {
-        logger.debug("getRankingshipshipmapsmap invoked");
+
+        logger.debug("getByShipAndMap invoked");
 
         List<CandidateInfo> result = recordServerServices.getEntriesByShipAndMap(map, ship);
 
         RankingEntry response = new RankingEntry();
 
         for (CandidateInfo toTransform : result) {
-            response.addRankingEntryItem(TRANSFORM_FUNCTION.apply(toTransform));
+            response.getRankingEntry().add(TRANSFORM_FUNCTION.apply(toTransform));
         }
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-    @Override
-    public ResponseEntity<NewMapEntryResponse> postRanking(@ApiParam(value = "New ranking request." ,required=true ) @RequestBody NewMapEntryRequest body) {
-        logger.debug("postRanking invoked");
-
-        final CandidateInfo uploadedCandidate = recordServerServices.addEntry(body.getPayload());
-
-        NewMapEntryResponse response = new NewMapEntryResponse().id(uploadedCandidate.getId()).position(1).status("OK").verified(true);
-
-        return new ResponseEntity<NewMapEntryResponse>(response, HttpStatus.OK);
+        return new ResponseEntity<RankingEntry>(response, HttpStatus.OK);
     }
 
     private static final Function<CandidateInfo, RankingEntryRankingEntry> TRANSFORM_FUNCTION
-            = new Function<CandidateInfo, RankingEntryRankingEntry>() {
-        @Override
-        public RankingEntryRankingEntry apply(final CandidateInfo f) {
-            return new RankingEntryRankingEntry()
-                    .position(f.getPosition())
-                    .time(f.getTime())
-                    .id(f.getId())
-                    .username(f.getUserName());
-        }
-    };
-
+            = f -> new RankingEntryRankingEntry()
+            .position(f.getPosition())
+            .time(f.getTime())
+            .id(f.getId())
+            .username(f.getUserName());
 }
